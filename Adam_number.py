acec70d6fca1b823da7c7ecd9dd1a8f71e240511
SAI GANESH <97424745+21A91A05C3@users.noreply.github.com>
2022-04-26 22:38:21 +0530

    a=int(input()) sq=a*a#144 d=0 d1=0 sum=0 rev=0 while(a>0):     d=a%10     sum=sum*10+d     a=a//10 sq1=sum*sum while(sq1>0):     d1=sq1%10     rev=rev*10+d1     sq1=sq1//10 if(sq==rev):     print("True") else:     print("False")

--- Adam_number.py
+++ Adam_number.py
@@ -1,19 +1,19 @@
 a=int(input())
-sq=a*a
+sq=a*a#144
+d=0
+d1=0
+sum=0
 rev=0
-rev1=0
 while(a>0):
     d=a%10
-    rev=rev*10+d
+    sum=sum*10+d
     a=a//10
-sq1=rev*rev
+sq1=sum*sum
 while(sq1>0):
     d1=sq1%10
-    rev1=rev1*10+d1
+    rev=rev*10+d1
     sq1=sq1//10
-if(sq==rev1):
+if(sq==rev):
     print("True")
 else:
     print("False")
-
-    
