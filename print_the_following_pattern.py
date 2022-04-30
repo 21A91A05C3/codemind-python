2c3fa13487e09b6ff9519754c11181a4de467f77
SAI GANESH <97424745+21A91A05C3@users.noreply.github.com>
2022-04-30 18:55:04 +0530

    n=int(input()) for i in range(1,n+1):#no.rows     for j in range(n,0,-1):#no.col         print(j,end=' ')     print('')

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,8 +1,5 @@
 n=int(input())
 for i in range(1,n+1):#no.rows
-    for j in range(1,n+1):#no.col
-        if(i==j):
-            print('0',end='')
-        else:
-            print('x',end='')
+    for j in range(n,0,-1):#no.col
+        print(j,end=' ')
     print('')
