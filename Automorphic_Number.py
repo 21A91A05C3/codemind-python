f65c35f522f1ff351fb45b65944506e34406f6bb
SAI GANESH <97424745+21A91A05C3@users.noreply.github.com>
2022-05-27 09:47:20 +0530

    n=int(input()) i=len(str(n)) sq=n**2 last=sq%pow(10,i) if last==n:     print("Automorphic Number") else:     print("Not an Automorphic Number")

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,8 +1,8 @@
-a=int(input())
-i=len(str(a))
-sq=a**2
+n=int(input())
+i=len(str(n))
+sq=n**2
 last=sq%pow(10,i)
-if last==a:
+if last==n:
     print("Automorphic Number")
 else:
     print("Not an Automorphic Number")
