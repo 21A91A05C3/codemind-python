933ca967121bdb455679dc7e7da6e77a8cf4fd3c
SAI GANESH <97424745+21A91A05C3@users.noreply.github.com>
2022-06-29 10:09:32 +0530

    def prime(i):     if i==1:         return 1     c=0     for j in range(2,(i//2)+1):         if i%j==0:             c+=1     if c==0:         return 1     else:         return 0 def palindrome(i):     if str(i)==str(i)[::-1]:         return 1     else:         return 0 n=int(input()) for i in range(n+1,n*n):     if prime(i)==1 and palindrome(i)==1:         print(i)         break

--- Program_to_find_out_the_next_prime_palindrome_number.py
+++ Program_to_find_out_the_next_prime_palindrome_number.py
@@ -1,19 +1,21 @@
-def prime(n):
-    if n==1:
-        return 0
-    c=0
-    for i in range(2,(n//2)+1):
-        if(n%i==0):
-            c+=1
-    if(c==0):
+def prime(i):
+    if i==1:
         return 1
-def pali(n):
-    if(str(n)==(str(n)[::-1])):
+    c=0
+    for j in range(2,(i//2)+1):
+        if i%j==0:
+            c+=1
+    if c==0:
+        return 1
+    else:
+        return 0
+def palindrome(i):
+    if str(i)==str(i)[::-1]:
         return 1
     else:
         return 0
 n=int(input())
 for i in range(n+1,n*n):
-    if(prime(i)==1 and pali(i)==1):
+    if prime(i)==1 and palindrome(i)==1:
         print(i)
         break
