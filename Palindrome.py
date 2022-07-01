9e1c3d2be7b5194bc11772878add9db6e850fcd1
SAI GANESH <97424745+21A91A05C3@users.noreply.github.com>
2022-07-01 12:57:52 +0530

    n=int(input()) k=str(n) k=k[::-1] if int(k)==int(n):     print("True") else:     print("False")

--- Palindrome.py
+++ Palindrome.py
@@ -1,12 +1,7 @@
-a=int(input())
-temp=a
-rev=0
-while(a>0):
-    d=a%10
-    rev=rev*10+d
-    a=a//10
-if(rev==temp):
+n=int(input())
+k=str(n)
+k=k[::-1]
+if int(k)==int(n):
     print("True")
 else:
     print("False")
-    
