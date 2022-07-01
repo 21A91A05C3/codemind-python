bc96b7838663b35903a10402b1dc27e491728823
SAI GANESH <97424745+21A91A05C3@users.noreply.github.com>
2022-07-01 11:49:02 +0530

    n=int(input()) a=0 b=1 print(a,b,end=" ") for i in range(n-2):     c=a+b     print(c,end=" ")     a=b     b=c

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,9 @@
 n=int(input())
 a=0
 b=1
-print(a,b,end=' ')
-sum=a+b
-for i in range(2,n):
-    print(sum,end=' ')
+print(a,b,end=" ")
+for i in range(n-2):
+    c=a+b
+    print(c,end=" ")
     a=b
-    b=sum
-    sum=a+b
+    b=c
