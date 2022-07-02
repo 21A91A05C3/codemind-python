14d41f5b41bf59d6a62c5bf8a788349e0c43b0ad
SAI GANESH <97424745+21A91A05C3@users.noreply.github.com>
2022-07-03 00:17:01 +0530

    k=int(input()) l=list(map(int,input().split())) a=[] for i in range(len(l)):     if l[i]%2==0:         a.append(l[i]) k=set(a) print(len(k))

--- unique_even_count.py
+++ unique_even_count.py
@@ -1,12 +1,8 @@
-n=int(input())
-Sum=0
-arr=list(map(int,input().strip().split()))[:n]
-for i in range(0,len(arr)):
-    c=0
-    for j in range(i+1,len(arr)):
-        if arr[i]==arr[j] and i!=j:
-            c+=1
-    if c==0:
-        if arr[i]%2==0:
-            Sum+=1
-print(Sum)
+k=int(input())
+l=list(map(int,input().split()))
+a=[]
+for i in range(len(l)):
+    if l[i]%2==0:
+        a.append(l[i])
+k=set(a)
+print(len(k))
