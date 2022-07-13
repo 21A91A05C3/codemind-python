d30b2ff0e9e6f53820b0c65fefd66a0f539cb66a
SAI GANESH <97424745+21A91A05C3@users.noreply.github.com>
2022-07-13 19:46:51 +0530

    n=input() m=input() for i in range(len(n)):     if n[i]==m:         print("True")         print(i)         break else:     print("False")

--- vowels_in_a_string.py
+++ vowels_in_a_string.py
@@ -1,10 +1,9 @@
 n=input()
 m=input()
-for i in range(0,len(n)-1):
-    if m in n:
+for i in range(len(n)):
+    if n[i]==m:
         print("True")
-        print(n.index(m))
+        print(i)
         break
-    
 else:
     print("False")
