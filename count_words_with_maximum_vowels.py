361484a8e8607fcb29e37cf8a68dc5ad158c0c13
SAI GANESH <97424745+21A91A05C3@users.noreply.github.com>
2022-07-14 11:24:22 +0530

    n=input().lower().split() l=[] k='aeiou' for i in n:     x=i     c=0     for i in x:         if i in k and i not in l:            c+=1     l.append(c) v=max(l) c=0 for i in l:     if v==i:         c+=1 print(c)

---
+++ count_words_with_maximum_vowels.py
@@ -0,0 +1,16 @@
+n=input().lower().split()
+l=[]
+k='aeiou'
+for i in n:             
+    x=i
+    c=0
+    for i in x:
+        if i in k and i not in l:
+           c+=1
+    l.append(c)
+v=max(l)
+c=0
+for i in l:
+    if v==i:
+        c+=1
+print(c)
