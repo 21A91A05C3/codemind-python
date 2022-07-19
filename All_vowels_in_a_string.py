522683b28d81d1125f416cca975b481eda0f7520
SAI GANESH <97424745+21A91A05C3@users.noreply.github.com>
2022-07-19 22:54:33 +0530

    n=input() c=0 l=[] for i in set(n):     if i in 'aeiou':         l.append(i)         c+=1 if  5==len(l):     print("True") else:     print("False") # print(l)

--- All_vowels_in_a_string.py
+++ All_vowels_in_a_string.py
@@ -1,12 +1,12 @@
 n=input()
-k='aeiou'
-l=[]
 c=0
-for i in n:
-    if i in k and i not in l:
+l=[]
+for i in set(n):
+    if i in 'aeiou':
         l.append(i)
         c+=1
-if c==len(k):
+if  5==len(l):
     print("True")
 else:
     print("False")
+# print(l)
