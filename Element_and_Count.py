4edb9401b7cd21cefe7734774f31d1dd5b708c15
SAI GANESH <97424745+21A91A05C3@users.noreply.github.com>
2022-07-19 10:03:12 +0530

    n=int(input()) l=list(map(int,input().split())) k=[] c=[] for i in l:     if i not in c:         c.append(i)         k.append(i)         k.append(l.count(i)) print(*k)

--- Element_and_Count.py
+++ Element_and_Count.py
@@ -1,10 +1,10 @@
-n=int(input()) 
-l=list(map(int,input().split())) 
-k=[] 
-c=[] 
-for i in l: 
+n=int(input())
+l=list(map(int,input().split()))
+k=[]
+c=[]
+for i in l:
     if i not in c:
-        c.append(i) 
-        k.append(i) 
-        k.append(l.count (i)) 
+        c.append(i)
+        k.append(i)
+        k.append(l.count(i))
 print(*k)
